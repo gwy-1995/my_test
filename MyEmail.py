2a5b67e1954f0e745584c82a2aedd6a9ede1b3d1
gwy-1995 <52060066+gwy-1995@users.noreply.github.com>
2020-02-20 03:34:07 +0800

    Update MyEmail.py

--- MyEmail.py
+++ MyEmail.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+a = input("输入邮件内容")
 
 mail_host = "smtp.qq.com"
 mail_username = "869790314@qq.com"
@@ -9,7 +10,7 @@ mail_password = "djgdhiznamtnbehc"
 sender = "869790314@qq.com"       #发件邮箱
 receivers = ["869790314@qq.com"]  #收件邮箱
 
-message = MIMEText("GWY邮件测试","plain","utf-8")   #内容
+message = MIMEText(a,"plain","utf-8")   #内容
 message['From'] = Header("869790314@qq.com", 'utf-8')   #发件人
 message['To'] = Header("GWY", 'utf-8')    #收件人
 
